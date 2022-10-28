c380b73635ef5a255ec4418c6b6bb8025b514791
Bhaskar gyan vardhan <bhaskarGyan@users.noreply.github.com>
2022-10-28 20:49:48 +0530

    inlineSSRStyles true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -59,8 +59,7 @@ export default defineNuxtConfig({
     }]
   ],
   experimental: {
-    reactivityTransform: true,
-    inlineSSRStyles: false,
+    reactivityTransform: true
   },
   runtimeConfig: {
     public: {
