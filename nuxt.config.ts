1b1d0f3a1f84985c13cda6785280fba67a5afe1b
Bhaskar gyan vardhan <bhaskarGyan@users.noreply.github.com>
2022-10-28 18:23:57 +0530

    update nuxt-cache-ssr to 1.0.5

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,32 +1,66 @@
 const isDev = process.env.NODE_ENV === 'development'
-
+import { hash as ohash } from 'ohash'
 // const apiBaseUrl = 'http://localhost:3001'
 const apiBaseUrl = 'https://movies-proxy.vercel.app'
-
+interface Device {
+  userAgent: string
+  isDesktop: boolean
+  isIos: boolean
+  isAndroid: boolean
+  isMobile: boolean
+  isMobileOrTablet: boolean
+  isDesktopOrTablet: boolean
+  isTablet: boolean
+  isWindows: boolean
+  isMacOS: boolean
+  isApple: boolean
+  isSafari: boolean
+  isFirefox: boolean
+  isEdge: boolean
+  isChrome: boolean
+  isSamsung: boolean
+  isCrawler: boolean
+}
 export default defineNuxtConfig({
   modules: [
     '@vueuse/nuxt',
     '@unocss/nuxt',
     '@pinia/nuxt',
     '@nuxt/image-edge',
-    ['nuxt-cache-ssr',{
-      enabled:true,
-      LRU:{
-        max:501,
-        ttl:1000 * 60
+    ['nuxt-cache-ssr', {
+      // Can be disable per enviroment, like in dev
+      enabled: true,
+      store: {
+        // Plceholder for store type, will be usable after Redis Release
+        type: 'memory',
+        // maximum number of pages to store in memory
+        // if limit is reached, least recently used page
+        // is removed.
+        max: 500,
+        // number of Millisecond to store this page in cache
+        ttl: 1000 * 60 // 1 Minute
       },
-      pages:[
+      pages: [
         '/movie',
-        '/tv'
-      ]
+        '/tv',
+        '/'
+      ],
+      key: (route: string, headers: any, device: Device) => {
+        const { userAgent, ...deviceType } = device
+        const key = [route];
+        Object.keys(deviceType).forEach(val => {
+          if (deviceType[val] && !val.includes('Tablet') && (val.includes('isMobile')  || val.includes('isDesktop')  || val.includes('isCrawler'))) {
+            key.push(val)
+          }
+        })
+        //  console.log(key)
+        return key.join("-")
+      }
     }]
   ],
   experimental: {
     reactivityTransform: true,
     inlineSSRStyles: false,
-  },
-  routeRules: {
-    '/**': isDev ? {} : { cache: { swr: true, headersOnly: true } },
   },
   runtimeConfig: {
     public: {
