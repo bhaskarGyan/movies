bef2a018f39f007f14de9631c47235b33417661d
Bhaskar gyan vardhan <bhaskarGyan@users.noreply.github.com>
2022-12-19 20:49:20 +0530

    Revert "remove preconnect"
    This reverts commit 17d5bbe1d03dae264746e6cda5cb60ae5e73bcb3.

--- app.vue
+++ app.vue
@@ -18,6 +18,16 @@ useHead({
   link: [
     {
       rel: 'icon', type: 'image/webp', href: '/movies.webp',
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://movies-proxy.vercel.app',
+      crossorigin:true
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.gstatic.com/',
+      crossorigin:true
     }
   ],
 })
